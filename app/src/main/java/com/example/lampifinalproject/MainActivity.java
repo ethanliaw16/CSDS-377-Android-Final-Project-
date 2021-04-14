de77a5fe5009b7063ec0dbf00f842e9399a1dd8f
Ethan Liaw <ethan.liaw@everykey.com>
2021-04-14 14:31:09 -0400

    formatting

--- app/src/main/java/com/example/lampifinalproject/MainActivity.java
+++ app/src/main/java/com/example/lampifinalproject/MainActivity.java
@@ -34,51 +34,47 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("Making request...");
         //RequestQueue queue = Volley.newRequestQueue(this);
         String url ="https://api.openweathermap.org/data/2.5/weather?q=Cleveland&appid=44f4936e6e18098c789b1ada6e7c6fbd";
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try{
-                            //System.out.println("Temperature: " + response.getJSONObject("main").getDouble("temp"));
-                            //System.out.println("Humidity: " + response.getJSONObject("main").getInt("humidity"));
-                            double temp = response.getJSONObject("main").getDouble("temp");
-                            temp -= 273.15;
-                            temp *= (9.0/5.0);
-                            temp += 32;
-                            int humidity = response.getJSONObject("main").getInt("humidity");
-                            String ec2Url = "http://34.226.146.171:8000/update-weather";
-                            JSONObject weatherPostData = new JSONObject();
-                            System.out.println("Adding " + temp + " as temp");
-                            weatherPostData.put("temp", temp);
-                            weatherPostData.put("humidity", humidity);
-                            JsonObjectRequest weatherPostRequest = new JsonObjectRequest(ec2Url, weatherPostData, new Response.Listener<JSONObject>() {
-                                @Override
-                                public void onResponse(JSONObject response) {
-                                    System.out.println(response);
-                                }
-                            }, new Response.ErrorListener() {
-
-                                @Override
-                                public void onErrorResponse(VolleyError error) {
-                                    // TODO: Handle error
-                                    System.out.println("ERROR :( " + error.getMessage());
-                                }
-                            });
-                            myQueue.add(weatherPostRequest);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try{
+                    //System.out.println("Temperature: " + response.getJSONObject("main").getDouble("temp"));
+                    //System.out.println("Humidity: " + response.getJSONObject("main").getInt("humidity"));
+                    double temp = response.getJSONObject("main").getDouble("temp");
+                    temp -= 273.15;
+                    temp *= (9.0/5.0);
+                    temp += 32;
+                    int humidity = response.getJSONObject("main").getInt("humidity");
+                    String ec2Url = "http://34.226.146.171:8000/update-weather";
+                    JSONObject weatherPostData = new JSONObject();
+                    System.out.println("Adding " + temp + " as temp");
+                    weatherPostData.put("temp", temp);
+                    weatherPostData.put("humidity", humidity);
+                    JsonObjectRequest weatherPostRequest = new JsonObjectRequest(ec2Url, weatherPostData, new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            System.out.println(response);
                         }
-                        catch (Exception e){
-                            System.out.println(e.getMessage());
+                        }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            // TODO: Handle error
+                            System.out.println("ERROR :( " + error.getMessage());
                         }
-                    }
-                }, new Response.ErrorListener() {
-
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // TODO: Handle error
-                        System.out.println("ERROR :( " + error.getMessage());
-                    }
-                });
+                    });
+                    myQueue.add(weatherPostRequest);
+                }
+                catch (Exception e){
+                    System.out.println(e.getMessage());
+                }
+            }
+            }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // TODO: Handle error
+                System.out.println("ERROR :( " + error.getMessage());
+            }
+        });
         myQueue.add(jsonObjectRequest);
     }
 }
