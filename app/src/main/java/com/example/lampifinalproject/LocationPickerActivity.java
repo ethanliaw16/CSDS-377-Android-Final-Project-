97b3b4e6d2f622d131851ac063e6897b6ce7e207
Ethan Liaw <ethan.liaw@everykey.com>
2021-04-26 18:10:00 -0400

    cleaned up location picker

--- app/src/main/java/com/example/lampifinalproject/LocationPickerActivity.java
+++ app/src/main/java/com/example/lampifinalproject/LocationPickerActivity.java
@@ -1,19 +1,82 @@
 package com.example.lampifinalproject;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class LocationPickerActivity extends FragmentActivity implements OnMapReadyCallback {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.PlaceLikelihood;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse;
+import com.google.android.libraries.places.api.net.PlacesClient;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+public class LocationPickerActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
 
     private GoogleMap mMap;
+    // New variables for Current Place Picker
+    private static final String TAG = "LocationPickerActivity";
+    ListView lstPlaces;
+    TextView locationText;
+    private PlacesClient mPlacesClient;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+
+    // The geographical location where the device is currently located. That is, the last-known
+    // location retrieved by the Fused Location Provider.
+    private Location mLastKnownLocation;
+
+    // A default location (Sydney, Australia) and default zoom to use when location permission is
+    // not granted.
+    private final LatLng mDefaultLocation = new LatLng(-33.8523341, 151.2106085);
+    private static final int DEFAULT_ZOOM = 15;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private boolean mLocationPermissionGranted;
+    private Marker locationMarker;
+    // Used for selecting the current place.
+    private static final int M_MAX_ENTRIES = 5;
+    private String[] mLikelyPlaceNames;
+    private String[] mLikelyPlaceAddresses;
+    private String[] mLikelyPlaceAttributions;
+    private LatLng[] mLikelyPlaceLatLngs;
+    private String currentState;
+    private String currentCity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +86,126 @@ public class LocationPickerActivity extends FragmentActivity implements OnMapRea
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+        // Set up the views
+        lstPlaces = findViewById(R.id.listPlaces);
+        locationText = findViewById(R.id.currentLocationText);
+        String apiKey = getString(R.string.google_maps_key);
+        Places.initialize(getApplicationContext(), apiKey);
+        mPlacesClient = Places.createClient(this);
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_geolocate:
+
+                // COMMENTED OUT UNTIL WE DEFINE THE METHOD
+                // Present the current place picker
+                // pickCurrentPlace();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    /**
+     * Handles the result of the request for location permissions.
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true;
+                }
+            }
+        }
+    }
+
+    private void getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        mLocationPermissionGranted = false;
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    public void useCurrentLocation(View view){
+        if (mLocationPermissionGranted) {
+            getDeviceLocation();
+        }
+    }
+
+    public void setLocation(View view){
+        Intent goToMain = new Intent(this, MainActivity.class);
+        goToMain.putExtra("lat", locationMarker.getPosition().latitude);
+        goToMain.putExtra("lon", locationMarker.getPosition().longitude);
+        goToMain.putExtra("city", currentCity);
+        goToMain.putExtra("state", currentState);
+        startActivity(goToMain);
+    }
+
+    private void getDeviceLocation(){
+        try {
+            if (mLocationPermissionGranted) {
+                Task<Location> locationResult = mFusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(this, new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        if (task.isSuccessful()) {
+                            // Set the map's camera position to the current location of the device.
+                            mLastKnownLocation = task.getResult();
+                            Log.d(TAG, "Latitude: " + mLastKnownLocation.getLatitude());
+                            Log.d(TAG, "Longitude: " + mLastKnownLocation.getLongitude());
+                            LatLng currentLocation = new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude());
+                            locationMarker.setPosition(currentLocation);
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                    new LatLng(mLastKnownLocation.getLatitude(),
+                                            mLastKnownLocation.getLongitude()), DEFAULT_ZOOM));
+                            updateLocationText();
+                        } else {
+                            Log.d(TAG, "Current location is null. Using defaults.");
+                            Log.e(TAG, "Exception: %s", task.getException());
+                            mMap.moveCamera(CameraUpdateFactory
+                                    .newLatLngZoom(mDefaultLocation, DEFAULT_ZOOM));
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
+        }
+
     }
 
     /**
@@ -39,8 +222,45 @@ public class LocationPickerActivity extends FragmentActivity implements OnMapRea
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng cleveland = new LatLng(41, -81);
+        locationMarker = mMap.addMarker(new MarkerOptions().position(cleveland).title("Your Currently Set Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(cleveland));
+
+        // Enable the zoom controls for the map
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        // Prompt the user for permission.
+        getLocationPermission();
+
+        mMap.setOnMapClickListener(this);
+
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        locationMarker.setPosition(latLng);
+        updateLocationText();
+    }
+
+    public void updateLocationText(){
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        try{
+            List<Address> addresses = geocoder.getFromLocation(locationMarker.getPosition().latitude, locationMarker.getPosition().longitude, 1);
+            String cityName = addresses.get(0).getAddressLine(0);
+            String stateName = addresses.get(0).getAddressLine(1);
+            String countryName = addresses.get(0).getAddressLine(2);
+            String[] addressElements = cityName.split(", ");
+            String extractedCityName = addressElements[addressElements.length - 3];
+            String extractedStateName = addressElements[addressElements.length - 2];
+            Log.i("Location","City is " + cityName);
+            Log.i("Location","Extracted city is " + extractedCityName);
+            Log.i("Location","State from is " + stateName);
+            currentCity = extractedCityName;
+            currentState = extractedStateName;
+            locationText.setText(cityName);
+
+        } catch (Exception e){
+            System.out.println("Error - " + e.getMessage());
+        }
+
     }
 }
--- app/src/main/java/com/example/lampifinalproject/MainActivity.java
+++ app/src/main/java/com/example/lampifinalproject/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -21,12 +23,17 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.maps.model.LatLng;
 
 import org.json.JSONObject;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Queue;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -34,14 +41,17 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     private Context context;
     private RequestQueue myQueue;
     private boolean notificationsOn = false;
+    private LatLng locationFromPicker;
     public static String INTENT_ACTION_NOTIFICATION = "com.example.lampifinalproject.notification";
     public static String NOTIFICATION_APP = "app";
     public static String NOTIFICATION_SENDER = "sender";
     public static String NOTIFICATION_MESSAGE = "message";
     public static final String WEATHER_API_TOKEN = "44f4936e6e18098c789b1ada6e7c6fbd";
-    public static final String WEATHER_API_BASE_URL = "https://api.openweathermap.org/data/2.5/weather?q=";
+    public static final String WEATHER_API_BASE_URL = "https://api.openweathermap.org/data/2.5/weather?lat=";
     private String[] allowedApps = {"Gmail","Messenger","Snapchat"};
     String[] cities = new String[]{"Cleveland,Ohio","Anchorage,Alaska","Lupin,Nunavut","Houston,Texas"};
+    HashMap<String, LatLng> cityLocations = new HashMap<>();
+    Queue<String> locationQueue = new ArrayDeque<>();
     private int currentlySelectedCity = 0;
     private List<String> allowedCities = new ArrayList<String>(Arrays.asList(cities));
     protected MyReceiver mReceiver = new MyReceiver();
@@ -55,13 +65,35 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     protected void onCreate(Bundle savedInstanceState) {
         context = getBaseContext();
         myQueue = Volley.newRequestQueue(this);
+        locationFromPicker = new LatLng(41, -81);
+        cityLocations.put("Default Location", locationFromPicker);
+        cities = cityLocations.keySet().toArray(new String[cityLocations.keySet().size()]);
         super.onCreate(savedInstanceState);
 
         Intent intent = new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
         startActivity(intent);
 
         setContentView(R.layout.activity_main);
+        Intent intentFromLocationPicker = getIntent();
+        if(intentFromLocationPicker != null){
+            double lat = intentFromLocationPicker.getDoubleExtra("lat", locationFromPicker.latitude);
+            double lon = intentFromLocationPicker.getDoubleExtra("lon", locationFromPicker.longitude);
+            String city = intentFromLocationPicker.getStringExtra("city");
+            String state = intentFromLocationPicker.getStringExtra("state");
+            String key = city + ", " + state;
+            locationFromPicker = new LatLng(lat,lon);
+            if(!cityLocations.containsKey(key) && city != null && state != null){
 
+                cityLocations.put(key, locationFromPicker);
+                locationQueue.add(city);
+                if(locationQueue.size() > 7){
+                    String keyToRemove = locationQueue.remove();
+                    cityLocations.remove(keyToRemove);
+                }
+                cities = cityLocations.keySet().toArray(new String[cityLocations.keySet().size()]);
+            }
+            Log.i("Location", "New location from location picker is " + locationFromPicker.toString());
+        }
         locationPicker = findViewById(R.id.simpleLocationPicker);
         ArrayAdapter<String> locationPickerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, cities);
         locationPicker.setAdapter(locationPickerAdapter);
@@ -103,12 +135,20 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         Log.i("Location picker", "Nothing Selected");
     }
 
+    public void goToLocationPicker(View view){
+        Intent intent = new Intent(context, LocationPickerActivity.class);
+        startActivity(intent);
+    }
+
     public void makeWeatherRequest(View view){
         Toast.makeText(getBaseContext(), "Makine request for weather", Toast.LENGTH_SHORT);
         // Instantiate the RequestQueue.
         System.out.println("Making request for " + cities[currentlySelectedCity]);
+        LatLng selectedLocation = cityLocations.get(cities[currentlySelectedCity]);
+
         //RequestQueue queue = Volley.newRequestQueue(this);
-        String url =WEATHER_API_BASE_URL + cities[currentlySelectedCity] + "&appid=" + WEATHER_API_TOKEN;//"https://api.openweathermap.org/data/2.5/weather?q=Cleveland&appid=44f4936e6e18098c789b1ada6e7c6fbd";
+        String url =WEATHER_API_BASE_URL + selectedLocation.latitude + "&lon=" + selectedLocation.longitude + "&appid=" + WEATHER_API_TOKEN;
+        //"https://api.openweathermap.org/data/2.5/weather?q=Cleveland&appid=44f4936e6e18098c789b1ada6e7c6fbd";
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
@@ -193,7 +233,9 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 String notificationApp = extras.getString(NOTIFICATION_APP);
                 String sender = extras.getString(NOTIFICATION_SENDER);
                 String message = extras.getString(NOTIFICATION_MESSAGE);
-
+                if(message.length() > 140){
+                    message = message.substring(0,140) + "...";
+                }
                 //System.out.println("Title: " + notificationApp);
                 //System.out.println("Content: " + sender);
                 //System.out.println("SubContent: " + message);
